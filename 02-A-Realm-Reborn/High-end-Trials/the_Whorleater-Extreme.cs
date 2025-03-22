617b5555afee5ed4304c3ca6a7bcb8e471942fdc
南沢响也 <72963826+Hibiya615@users.noreply.github.com>
2025-03-23 04:22:56 +0800

    提交极水神半成品

--- 02-A-Realm-Reborn/High-end-Trials/the_Whorleater-Extreme.cs
+++ 02-A-Realm-Reborn/High-end-Trials/the_Whorleater-Extreme.cs
@@ -14,6 +14,7 @@ using ECommons;
 using ECommons.DalamudServices;
 using ECommons.GameFunctions;
 using ECommons.MathHelpers;
+using System.Threading.Tasks;
 
 namespace the_Whorleater_Extreme;
 
@@ -29,6 +30,40 @@ public class the_Whorleater_Extreme
         """;
     
     
+    // 阶段
+    // P1：被光明所污染的人们，你们竟敢侵入我的领域！
+    // P2：蓝色清净的大海之水将会扫去一切污秽！
+    // P3：竟然对我卖弄小伎俩？
+    
+    
+    uint Dive = 0;
+    
+    public void Init(ScriptAccessory accessory) {
+        Dive = 0;
+    }
+    
+    [ScriptMethod(name: "下潜记录", eventType: EventTypeEnum.Targetable, eventCondition: ["SourceName:regex:^(利维亚桑|Leviathan|リヴァイアサン)$", "Targetable:False"],userControl: false)]
+    public void 下潜记录(Event @event, ScriptAccessory accessory)
+    {
+        Dive = 1;
+    }
+    
+    [ScriptMethod(name: "下潜取消", eventType: EventTypeEnum.Targetable, eventCondition: ["SourceName:regex:^(利维亚桑|Leviathan|リヴァイアサン)$", "Targetable:True"],userControl: false)]
+    public void 下潜取消(Event @event, ScriptAccessory accessory)
+    {
+        Dive = 0;
+    }
+    
+    
+    [ScriptMethod(name: "开场提示", eventType: EventTypeEnum.Countdown, eventCondition: ["Type:Stop","SourceId:E0000000"])]
+    public async void 开场提示(Event @event, ScriptAccessory accessory)
+    {
+        await Task.Delay(3000); 
+        accessory.Method.TextInfo("难度：★★，TH不会建议退，小抄已发至聊天框 \nT：MT拉头，ST拉尾、小怪ST拉，晕波齿鱼人（60%血以下免晕）\nD：出黄球打黄球，小怪优先波齿鱼人，注意避开T的平A \nH：ST交给小仙女和再生奶，群抬错开ST，尽量避免水镜debuff"
+            , duration: 15000, true);
+        accessory.Method.SendChat("/e ————小抄————\nT：MT拉头，ST拉尾、小怪ST拉，晕波齿鱼人（60%血以下免晕）\n蓝球ST拉着溜，等一次拍击后拉离人群开大减炸掉 \nD：出黄球打黄球，小怪优先波齿鱼人，注意避开T的平A \nH：ST交给小仙女和再生奶，群抬错开ST，尽量避免水镜debuff");
+    }
+    
     [ScriptMethod(name: "猛撞销毁", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:2090"],userControl: false)]
     public void 猛撞销毁(Event @event, ScriptAccessory accessory)
     {
@@ -38,13 +73,28 @@ public class the_Whorleater_Extreme
     [ScriptMethod(name: "水神的面纱 提示", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:2165"])]
     public void 水神的面纱(Event @event, ScriptAccessory accessory)
     {
-        accessory.Method.TextInfo("法系打头部，物理打尾巴\n龙骑舞者赤魔DK额外注意自己的技能！\n尾巴有身位！", duration: 10000, true);
+        accessory.Method.TextInfo("法系打头部，物理打尾巴\n龙骑舞者赤魔DK额外注意自己的技能！\n尾巴有身位！", duration: 5000, true);
     }
     
     [ScriptMethod(name: "波齿鱼人 击杀提示", eventType: EventTypeEnum.AddCombatant, eventCondition: ["DataId:2807"])]
     public void 波齿鱼人提示(Event @event, ScriptAccessory accessory)
     {
         accessory.Method.TextInfo("优先击杀 < 波齿鱼人 >", duration: 2500, true);
+    }
+    
+    [ScriptMethod(name: "波齿鱼人_恐慌风暴", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:1865"])]
+    public void 恐慌风暴(Event @event, ScriptAccessory accessory)
+    {
+        accessory.Method.EdgeTTS("不要踩进恐慌圈");
+        
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "恐慌风暴";
+        dp.Color = new Vector4(1f, 0f, 1f, 1.5f);
+        dp.Position = @event.EffectPosition();
+        dp.Scale = new Vector2(6f);
+        dp.DestoryAt = 6000;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Circle, dp);
+        
     }
     
     [ScriptMethod(name: "波齿鱼人_恐慌洗礼", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:1866"])]
@@ -54,7 +104,7 @@ public class the_Whorleater_Extreme
         
         var dp = accessory.Data.GetDefaultDrawProperties();
         dp.Name = "恐慌洗礼";
-        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Color = new Vector4(1f, 0f, 1f, 2f);
         dp.Owner = @event.SourceId();
         dp.Scale = new Vector2(1.5f);
         dp.DestoryAt = 3200;
@@ -67,6 +117,46 @@ public class the_Whorleater_Extreme
         accessory.Method.RemoveDraw("恐慌洗礼");
     }
     
+    [ScriptMethod(name: "下潜提示", eventType: EventTypeEnum.Targetable, eventCondition: ["SourceName:regex:^(利维亚桑|Leviathan|リヴァイアサン)$", "Targetable:False"])]
+    public void 下潜提示(Event @event, ScriptAccessory accessory)
+    {
+        accessory.Method.TextInfo("中间集合，躲开南北俯冲，远离东西水柱击退", duration: 5000, true);
+    }
+    
+    [ScriptMethod(name: "旋转下潜 俯冲", eventType: EventTypeEnum.AddCombatant, eventCondition: ["DataId:2804"])]
+    public void 旋转下潜(Event @event, ScriptAccessory accessory)
+    {
+        if ( Dive != 1) return;  
+        
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "旋转下潜";
+        dp.Scale = new (16, 40f);
+        dp.Owner = @event.SourceId();
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.DestoryAt = 6200;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp);  
+    }
+    
+    [ScriptMethod(name: "旋转下潜销毁", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:2186"],userControl: false)]
+    public void 旋转下潜销毁(Event @event, ScriptAccessory accessory)
+    {
+        accessory.Method.RemoveDraw("旋转下潜");
+    }
+    
+    // 巨浪击退距离 25m ， 场地长 40m
+    
+    [ScriptMethod(name: "巨浪销毁", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:2094"],userControl: false)]
+    public void 巨浪销毁(Event @event, ScriptAccessory accessory)
+    {
+        accessory.Method.RemoveDraw("巨浪");
+    }
+    
+    [ScriptMethod(name: "巨浪泡沫 提示", eventType: EventTypeEnum.AddCombatant, eventCondition: ["DataId:2810"])]
+    public void 巨浪泡沫(Event @event, ScriptAccessory accessory)
+    {
+        accessory.Method.TextInfo("ST接走蓝泡泡，避开人群溜溜球，转场完开减伤远离人群炸球", duration: 34500, false);
+        accessory.Method.EdgeTTS("ST拉蓝球，约半分钟后爆炸");
+    }
     
 }
 
