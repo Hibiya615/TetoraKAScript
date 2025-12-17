b36b95c2f0be7d387e23858c2269e0d105616cbd
南沢响也 <72963826+Hibiya615@users.noreply.github.com>
2025-12-18 00:05:34 +0800

    更新复制粘贴用文本

--- 02-A-Realm-Reborn/Trials/DhormeChimera.cs
+++ 02-A-Realm-Reborn/Trials/DhormeChimera.cs
@@ -9,7 +9,18 @@ using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent;
 using KodakkuAssist.Module.Draw;
 using KodakkuAssist.Data;
+using KodakkuAssist.Extensions;
 using System.Threading.Tasks;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Collections.Generic;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace DhormeChimera;
 
@@ -24,6 +35,8 @@ public class DhormeChimera
         LV50 死化奇美拉讨伐战 初版绘制
         """;
     
+    #region 
+
     [UserSetting("TTS开关（TTS请二选一开启）")]
     public bool isTTS { get; set; } = false;
     
@@ -32,6 +45,8 @@ public class DhormeChimera
     
     [UserSetting("弹窗文本提示开关")]
     public bool isText { get; set; } = true;
+    
+    #endregion
     
     uint StaticCharge=0; // 带电 StatusID 326
     uint GelidCharge=0; // 带冰 StatusID 327
@@ -46,7 +61,7 @@ public class DhormeChimera
     {
         StaticCharge = 1;
         GelidCharge = 0;
-        accessory.Method.RemoveDraw("寒冰吐息");
+        accessory.Method.RemoveDraw($"寒冰吐息");
     }
     
     [ScriptMethod(name:"带冰记录", eventType:EventTypeEnum.ActionEffect, eventCondition: ["ActionId:1284"], userControl:false)]
@@ -54,26 +69,26 @@ public class DhormeChimera
     {
         StaticCharge = 0;
         GelidCharge = 1;
-        accessory.Method.RemoveDraw("雷电吐息");
+        accessory.Method.RemoveDraw($"雷电吐息");
     }
     
     [ScriptMethod(name: "伤头&插言 打断销毁", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:regex:^75(38|51)$"], userControl: false)]
     public void 打断销毁(Event @event, ScriptAccessory accessory)
     {
-        accessory.Method.RemoveDraw("(寒冰|雷电)咆哮");
+        accessory.Method.RemoveDraw($"(寒冰|雷电)咆哮");
     }
     
     [ScriptMethod(name: "寒冰&雷电咆哮 打断销毁", eventType: EventTypeEnum.CancelAction, eventCondition: ["ActionId:regex:^(1104|1338|1442)$"], userControl: false)]
     public void 寒冰雷电咆哮销毁(Event @event, ScriptAccessory accessory)
     {
-        accessory.Method.RemoveDraw("(寒冰|雷电)咆哮");
+        accessory.Method.RemoveDraw($"(寒冰|雷电)咆哮");
     }
     
     [ScriptMethod(name: "雷电吐息", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:1283"])]
     public void 雷电吐息(Event @event, ScriptAccessory accessory)
     {
         var dp = accessory.Data.GetDefaultDrawProperties();
-        dp.Name = "雷电吐息";
+        dp.Name = $"雷电吐息";
         dp.Color = new Vector4(1f, 0.2f, 1f, 0.4f);
         dp.Owner = @event.SourceId();
         dp.Scale = new Vector2(9.7f);
@@ -87,7 +102,7 @@ public class DhormeChimera
     public void 寒冰吐息(Event @event, ScriptAccessory accessory)
     {
         var dp = accessory.Data.GetDefaultDrawProperties();
-        dp.Name = "寒冰吐息";
+        dp.Name = $"寒冰吐息";
         dp.Color = new Vector4(0f, 1f, 1f, 0.4f);
         dp.Owner = @event.SourceId();
         dp.Scale = new Vector2(9.7f);
@@ -101,16 +116,16 @@ public class DhormeChimera
     public void 嘈杂的噪音(Event @event, ScriptAccessory accessory)
     {
         if ( @event.TargetId() != accessory.Data.Me) return; 
-        if (isText)accessory.Method.TextInfo("避开人群溜雷球", duration: 3500, true);
-        if (isTTS)accessory.Method.TTS("雷球点名");
-        if (isEdgeTTS)accessory.Method.EdgeTTS("雷球点名");
+        if (isText)accessory.Method.TextInfo($"避开人群溜雷球", duration: 3500, true);
+        if (isTTS)accessory.Method.TTS($"雷球点名");
+        if (isEdgeTTS)accessory.Method.EdgeTTS($"雷球点名");
     }
     
     [ScriptMethod(name: "嘈杂的噪音（雷球）", eventType: EventTypeEnum.AddCombatant, eventCondition: ["DataId:2222"])]
     public void 无序的和声(Event @event, ScriptAccessory accessory)
     {
         var dp = accessory.Data.GetDefaultDrawProperties();
-        dp.Name = "无序的和声";
+        dp.Name = $"无序的和声";
         dp.Color = accessory.Data.DefaultDangerColor;
         dp.Owner = @event.SourceId();
         dp.Scale = new Vector2(6f);
@@ -122,14 +137,14 @@ public class DhormeChimera
     [ScriptMethod(name: "无序的和声销毁", eventType: EventTypeEnum.ActionEffect, eventCondition: ["ActionId:1108"],userControl: false)]
     public void 无序的和声销毁(Event @event, ScriptAccessory accessory)
     {
-        accessory.Method.RemoveDraw("无序的和声");
+        accessory.Method.RemoveDraw($"无序的和声");
     }
     
     [ScriptMethod(name: "寒冰咆哮（钢铁）", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:1104"])]
     public void 寒冰咆哮(Event @event, ScriptAccessory accessory)
     {
         var dp = accessory.Data.GetDefaultDrawProperties();
-        dp.Name = "寒冰咆哮";
+        dp.Name = $"寒冰咆哮";
         dp.Color = accessory.Data.DefaultDangerColor;
         dp.Owner = @event.SourceId();
         dp.Scale = new Vector2(6f);
@@ -140,16 +155,16 @@ public class DhormeChimera
     [ScriptMethod(name: "强化寒冰咆哮 提示", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:1285"])]
     public void 强化寒冰咆哮(Event @event, ScriptAccessory accessory)
     {
-        if (isText)accessory.Method.TextInfo("打断或眩晕BOSS", duration: 2000, true);
-        if (isTTS)accessory.Method.TTS("打断或眩晕BOSS");
-        if (isEdgeTTS)accessory.Method.EdgeTTS("打断或眩晕BOSS");
+        if (isText)accessory.Method.TextInfo($"打断或眩晕BOSS", duration: 2000, true);
+        if (isTTS)accessory.Method.TTS($"打断或眩晕BOSS");
+        if (isEdgeTTS)accessory.Method.EdgeTTS($"打断或眩晕BOSS");
     }
     
     [ScriptMethod(name: "雷电咆哮（月环）", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:regex:^(1338|1442)$"])]
     public void 雷电咆哮(Event @event, ScriptAccessory accessory)
     {
         var dp = accessory.Data.GetDefaultDrawProperties();
-        dp.Name = "雷电咆哮";
+        dp.Name = $"雷电咆哮";
         dp.Color = accessory.Data.DefaultDangerColor;
         dp.Owner = @event.SourceId();
         dp.Scale = new Vector2(30f);
@@ -295,3 +310,315 @@ public static class MathHelpers
         return radians * 180.0 / Math.PI;
     }
 }
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        var attribute = field?.GetCustomAttributes(typeof(DescriptionAttribute), false)
+            .FirstOrDefault() as DescriptionAttribute;
+        return attribute?.Description ?? value.ToString();
+    }
+}
+
+public static class MathTools
+{
+    public static float DegToRad(this float deg) => (deg + 360f) % 360f / 180f * float.Pi;
+    public static float RadToDeg(this float rad) => (rad + 2 * float.Pi) % (2 * float.Pi) / float.Pi * 180f;
+
+    /// <summary>
+    /// 获得任意点与中心点的弧度值，以(0, 0, 1)方向为0，以(1, 0, 0)方向为pi/2。
+    /// 即，逆时针方向增加。
+    /// </summary>
+    /// <param name="point">任意点</param>
+    /// <param name="center">中心点</param>
+    /// <returns></returns>
+    public static float GetRadian(this Vector3 point, Vector3 center)
+        => MathF.Atan2(point.X - center.X, point.Z - center.Z);
+
+    /// <summary>
+    /// 获得任意点与中心点的长度。
+    /// </summary>
+    /// <param name="point">任意点</param>
+    /// <param name="center">中心点</param>
+    /// <returns></returns>
+    public static float GetLength(this Vector3 point, Vector3 center)
+        => new Vector2(point.X - center.X, point.Z - center.Z).Length();
+
+    /// <summary>
+    /// 将任意点以中心点为圆心，逆时针旋转并延长。
+    /// </summary>
+    /// <param name="point">任意点</param>
+    /// <param name="center">中心点</param>
+    /// <param name="radian">旋转弧度</param>
+    /// <param name="length">基于该点延伸长度</param>
+    /// <returns></returns>
+    public static Vector3 RotateAndExtend(this Vector3 point, Vector3 center, float radian, float length)
+    {
+        var baseRad = point.GetRadian(center);
+        var baseLength = point.GetLength(center);
+        var rotRad = baseRad + radian;
+        return new Vector3(
+            center.X + MathF.Sin(rotRad) * (length + baseLength),
+            center.Y,
+            center.Z + MathF.Cos(rotRad) * (length + baseLength)
+        );
+    }
+
+    /// <summary>
+    /// 获得某角度所在划分区域
+    /// </summary>
+    /// <param name="radian">输入弧度</param>
+    /// <param name="regionNum">区域划分数量</param>
+    /// <param name="baseRegionIdx">0度所在区域的初始Idx</param>>
+    /// <param name="isDiagDiv">是否为斜分割，默认为false</param>
+    /// <param name="isCw">是否顺时针增加，默认为false</param>
+    /// <returns></returns>
+    public static int RadianToRegion(this float radian, int regionNum, int baseRegionIdx = 0, bool isDiagDiv = false, bool isCw = false)
+    {
+        var sepRad = float.Pi * 2 / regionNum;
+        var inputAngle = radian * (isCw ? -1 : 1) + (isDiagDiv ? sepRad / 2 : 0);
+        var rad = (inputAngle + 4 * float.Pi) % (2 * float.Pi);
+        return ((int)Math.Floor(rad / sepRad) + baseRegionIdx + regionNum) % regionNum;
+    }
+
+    /// <summary>
+    /// 将输入点左右折叠
+    /// </summary>
+    /// <param name="point">待折叠点</param>
+    /// <param name="centerX">中心折线坐标点</param>
+    /// <returns></returns>
+    public static Vector3 FoldPointHorizon(this Vector3 point, float centerX)
+        => point with { X = 2 * centerX - point.X };
+
+    /// <summary>
+    /// 将输入点上下折叠
+    /// </summary>
+    /// <param name="point">待折叠点</param>
+    /// <param name="centerZ">中心折线坐标点</param>
+    /// <returns></returns>
+    public static Vector3 FoldPointVertical(this Vector3 point, float centerZ)
+        => point with { Z = 2 * centerZ - point.Z };
+
+    /// <summary>
+    /// 将输入点中心对称
+    /// </summary>
+    /// <param name="point">输入点</param>
+    /// <param name="center">中心点</param>
+    /// <returns></returns>
+    public static Vector3 PointCenterSymmetry(this Vector3 point, Vector3 center)
+        => point.RotateAndExtend(center, float.Pi, 0);
+
+    /// <summary>
+    /// 获取给定数的指定位数
+    /// </summary>
+    /// <param name="val">给定数值</param>
+    /// <param name="x">对应位数，个位为1</param>
+    /// <returns></returns>
+    public static int GetDecimalDigit(this int val, int x)
+    {
+        var valStr = val.ToString();
+        var length = valStr.Length;
+        if (x < 1 || x > length) return -1;
+        var digitChar = valStr[length - x]; // 从右往左取第x位
+        return int.Parse(digitChar.ToString());
+    }
+}
+
+public enum MarkType
+{
+    None = -1,
+    Attack1 = 0,
+    Attack2 = 1,
+    Attack3 = 2,
+    Attack4 = 3,
+    Attack5 = 4,
+    Bind1 = 5,
+    Bind2 = 6,
+    Bind3 = 7,
+    Ignore1 = 8,
+    Ignore2 = 9,
+    Square = 10,
+    Circle = 11,
+    Cross = 12,
+    Triangle = 13,
+    Attack6 = 14,
+    Attack7 = 15,
+    Attack8 = 16,
+    Count = 17
+}
+
+public static class IbcHelper
+{
+    public static IGameObject? GetById(this ScriptAccessory sa, ulong gameObjectId)
+    {
+        return sa.Data.Objects.SearchById(gameObjectId);
+    }
+
+    public static IGameObject? GetMe(this ScriptAccessory sa)
+    {
+        return sa.Data.Objects.LocalPlayer;
+    }
+
+    public static IEnumerable<IGameObject?> GetByDataId(this ScriptAccessory sa, uint dataId)
+    {
+        return sa.Data.Objects.Where(x => x.DataId == dataId);
+    }
+
+    public static string GetPlayerJob(this ScriptAccessory sa, IPlayerCharacter? playerObject, bool fullName = false)
+    {
+        if (playerObject == null) return "None";
+        return fullName ? playerObject.ClassJob.Value.Name.ToString() : playerObject.ClassJob.Value.Abbreviation.ToString();
+    }
+
+    public static float GetStatusRemainingTime(this ScriptAccessory sa, IBattleChara? battleChara, uint statusId)
+    {
+        if (battleChara == null || !battleChara.IsValid()) return 0;
+        unsafe
+        {
+            BattleChara* charaStruct = (BattleChara*)battleChara.Address;
+            var statusIdx = charaStruct->GetStatusManager()->GetStatusIndex(statusId);
+            return charaStruct->GetStatusManager()->GetRemainingTime(statusIdx);
+        }
+    }
+
+    public static bool HasStatus(this ScriptAccessory sa, IBattleChara? battleChara, uint statusId)
+    {
+        if (battleChara == null || !battleChara.IsValid()) return false;
+        unsafe
+        {
+            BattleChara* charaStruct = (BattleChara*)battleChara.Address;
+            var statusIdx = charaStruct->GetStatusManager()->GetStatusIndex(statusId);
+            return statusIdx != -1;
+        }
+    }
+
+    /// <summary>
+    /// 获取指定标记索引的对象EntityId
+    /// </summary>
+    public static unsafe ulong GetMarkerEntityId(uint markerIndex)
+    {
+        var markingController = MarkingController.Instance();
+        if (markingController == null) return 0;
+        if (markerIndex >= 17) return 0;
+
+        return markingController->Markers[(int)markerIndex];
+    }
+
+    /// <summary>
+    /// 获取对象身上的标记
+    /// </summary>
+    /// <returns>MarkType</returns>
+    public static MarkType GetObjectMarker(IGameObject? obj)
+    {
+        if (obj == null || !obj.IsValid()) return MarkType.None;
+
+        ulong targetEntityId = obj.EntityId;
+            
+        for (uint i = 0; i < 17; i++)
+        {
+            var markerEntityId = GetMarkerEntityId(i);
+            if (markerEntityId == targetEntityId)
+            {
+                return (MarkType)i;
+            }
+        }
+
+        return MarkType.None;
+    }
+
+    /// <summary>
+    /// 检查对象是否有指定的标记
+    /// </summary>
+    public static bool HasMarker(IGameObject? obj, MarkType markType)
+    {
+        return GetObjectMarker(obj) == markType;
+    }
+
+    /// <summary>
+    /// 检查对象是否有任何标记
+    /// </summary>
+    public static bool HasAnyMarker(IGameObject? obj)
+    {
+        return GetObjectMarker(obj) != MarkType.None;
+    }
+
+    private static ulong GetMarkerForObject(IGameObject? obj)
+    {
+        if (obj == null) return 0;
+        unsafe
+        {
+            for (uint i = 0; i < 17; i++)
+            {
+                var markerEntityId = GetMarkerEntityId(i);
+                if (markerEntityId == obj.EntityId)
+                {
+                    return markerEntityId;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static MarkType GetMarkerTypeForObject(IGameObject? obj)
+    {
+        if (obj == null) return MarkType.None;
+        unsafe
+        {
+            for (uint i = 0; i < 17; i++)
+            {
+                var markerEntityId = GetMarkerEntityId(i);
+                if (markerEntityId == obj.EntityId)
+                {
+                    return (MarkType)i;
+                }
+            }
+        }
+        return MarkType.None;
+    }
+
+    /// <summary>
+    /// 获取标记的名称
+    /// </summary>
+    public static string GetMarkerName(MarkType markType)
+    {
+        return markType switch
+        {
+            MarkType.Attack1 => "攻击1",
+            MarkType.Attack2 => "攻击2",
+            MarkType.Attack3 => "攻击3",
+            MarkType.Attack4 => "攻击4",
+            MarkType.Attack5 => "攻击5",
+            MarkType.Bind1 => "止步1",
+            MarkType.Bind2 => "止步2",
+            MarkType.Bind3 => "止步3",
+            MarkType.Ignore1 => "禁止1",
+            MarkType.Ignore2 => "禁止2",
+            MarkType.Square => "方块",
+            MarkType.Circle => "圆圈",
+            MarkType.Cross => "十字",
+            MarkType.Triangle => "三角",
+            MarkType.Attack6 => "攻击6",
+            MarkType.Attack7 => "攻击7",
+            MarkType.Attack8 => "攻击8",
+            _ => "无标记"
+        };
+    }
+    
+    public static float GetHitboxRadius(IGameObject obj)
+    {
+        if (obj == null || !obj.IsValid()) return -1;
+        return obj.HitboxRadius;
+    }
+
+}
+
+public static class HelperExtensions
+{
+    public static unsafe uint GetCurrentTerritoryId()
+    {
+        return AgentMap.Instance()->CurrentTerritoryId; // 额外进行地图ID判断
+    }
+}
+
